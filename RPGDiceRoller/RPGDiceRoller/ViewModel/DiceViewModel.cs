ce17d4a313bdde18315b6b94328fe4d5c337dbdd
Jordan Svancara <jakalal@live.com>
2017-04-26 14:38:36 -0500

    Adds number of dice UI controls

--- RPGDiceRoller/RPGDiceRoller/ViewModel/DiceViewModel.cs
+++ RPGDiceRoller/RPGDiceRoller/ViewModel/DiceViewModel.cs
@@ -13,14 +13,31 @@ namespace RPGDiceRoller.ViewModel
 {
     public class DiceViewModel : INotifyPropertyChanged
     {
-        public Command RollD4Command { get; set; }
-        public Command RollD6Command { get; set; }
-        public Command RollD8Command { get; set; }
-        public Command RollD10Command { get; set; }
-        public Command RollD12Command { get; set; }
-        public Command RollD20Command { get; set; }
-        public Command RollD100Command { get; set; }
-        private ObservableCollection<Rolls> rolls = new ObservableCollection<Model.Rolls>();
+
+        public Command RollDieCommand { get; set; }
+        public Command MinusNumDiceCommand { get; set; }
+        public Command PlusNumDiceCommand { get; set; }
+        private Random random = new Random();
+        private int numDice = 1;
+        private ObservableCollection<Rolls> rolls = new ObservableCollection<Rolls>();
+
+        public DiceViewModel()
+        {
+            RollDieCommand = new Command<string>(async (x) => await RollDie(x));
+            MinusNumDiceCommand = new Command(MinusNumDice);
+            PlusNumDiceCommand = new Command(PlusNumDice);
+        }
+
+        private void PlusNumDice(object obj)
+        {
+            NumDice += 1;
+        }
+
+        private void MinusNumDice()
+        {
+            NumDice -= 1;
+        }
+
         public ObservableCollection<Rolls> Rolls
 
         {
@@ -31,18 +48,17 @@ namespace RPGDiceRoller.ViewModel
                 OnPropertyChanged();
             }
         }
-        Random random = new Random();
-
-        public DiceViewModel()
+        public int NumDice
         {
-            RollD4Command = new Command(async () => await RollD4());
-            RollD6Command = new Command(async () => await RollD6());
-            RollD8Command = new Command(async () => await RollD8());
-            RollD10Command = new Command(async () => await RollD10());
-            RollD12Command = new Command(async () => await RollD12());
-            RollD20Command = new Command(async () => await RollD20());
-            RollD100Command = new Command(async () => await RollD100());
+            get { return numDice; }
+            set
+            {
+                numDice = value;
+                OnPropertyChanged();
+            }
         }
+
+        
 
         int latestRoll;
         public int LatestRoll
@@ -56,42 +72,22 @@ namespace RPGDiceRoller.ViewModel
         }
 
         
-
-
-        async Task RollD4()
+        async Task RollDie(string sideCountString)
         {
-            LatestRoll = random.Next(1, 5);
-            Rolls.Insert(0,new Rolls(4, LatestRoll));
-        }
-        async Task RollD6()
-        {
-            LatestRoll = random.Next(1, 7);
-            Rolls.Insert(0, new Rolls(6, LatestRoll));
-        }
-        async Task RollD8()
-        {
-            LatestRoll = random.Next(1, 9);
-            Rolls.Insert(0, new Rolls(8, LatestRoll));
-        }
-        async Task RollD10()
-        {
-            LatestRoll = random.Next(1, 11);
-            Rolls.Insert(0, new Rolls(10, LatestRoll));
-        }
-        async Task RollD12()
-        {
-            LatestRoll = random.Next(1, 13);
-            Rolls.Insert(0, new Rolls(12, LatestRoll));
-        }
-        async Task RollD20()
-        {
-            LatestRoll = random.Next(1, 21);
-            Rolls.Insert(0, new Rolls(20, LatestRoll));
-        }
-        async Task RollD100()
-        {
-            LatestRoll = random.Next(1, 101);
-            Rolls.Insert(0, new Rolls(100, LatestRoll));
+            try
+            {
+                int sideCount = Convert.ToInt32(sideCountString);
+                LatestRoll = random.Next(1, sideCount + 1);
+                Rolls.Insert(0, new Rolls(sideCount, LatestRoll));
+            }
+            catch (FormatException e)
+            {
+                throw new FormatException("Input string is not a sequence of digits.", e);
+            }
+            catch (OverflowException e)
+            {
+                throw new OverflowException("The number cannot fit in an Int32.", e);
+            }
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
